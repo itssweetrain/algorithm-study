3cb70f91a8d61e9fd9cba11dd26bfc39bce9d840
Danbi <76150928+itssweetrain@users.noreply.github.com>
2023-04-16 01:00:37 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.15 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.kt
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.kt
@@ -5,34 +5,12 @@ class Solution {
         var answer: Int = 0
         var strArr = arrayListOf<String>()
         
-        fun stackEle(arr: ArrayList<String>): Boolean {
-           val stack = Stack<String>()
-           for(i in arr) {
-                val s = i.toString()
-                if(s == "[" || s == "{" || s == "(") {
-                    stack.push(s)
-                } else {
-                    if(!stack.isEmpty()) {
-                        when(s) {
-                            "]" -> if(stack.peek() == "[") stack.pop()
-                            "}" -> if(stack.peek() == "{") stack.pop() 
-                            ")" -> if(stack.peek() == "(") stack.pop() 
-                            else -> stack.push(s)
-                        }
-                    } else {
-                        stack.push(s)
-                    }
-                }
-            }
-           return stack.isEmpty()
-        }
-
-       for(i in s) { 
+        for(i in s) { 
            strArr.add(i.toString()) 
         }
-        
-       for(i in s) {
-           if(stackEle(strArr)) {
+
+        for(i in s) {
+           if(isValidate(strArr)) {
                 answer ++
             }
             strArr.add(strArr[0].toString())   
@@ -40,5 +18,27 @@ class Solution {
         }     
          
         return answer
+    }
+    
+    fun isValidate(arr: ArrayList<String>): Boolean {
+       val stack = Stack<String>()
+       for(i in arr) {
+            val s = i.toString()
+            if(s == "[" || s == "{" || s == "(") {
+                stack.push(s)
+            } else {
+                if(!stack.isEmpty()) {
+                    when(s) {
+                        "]" -> if(stack.peek() == "[") stack.pop()
+                        "}" -> if(stack.peek() == "{") stack.pop() 
+                        ")" -> if(stack.peek() == "(") stack.pop() 
+                        else -> stack.push(s)
+                    }
+                } else {
+                    stack.push(s)
+                }
+            }
+        }
+       return stack.isEmpty()
     }
 }
