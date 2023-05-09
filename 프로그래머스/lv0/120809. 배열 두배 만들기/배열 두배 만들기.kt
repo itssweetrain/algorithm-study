f42b7918f75885295e2cc58f3bd67aae1a9823fe
Danbi <76150928+itssweetrain@users.noreply.github.com>
2023-05-09 23:50:12 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.kt
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray): ArrayList<Int> {
-        var result: ArrayList<Int> = arrayListOf()
-        for(i in numbers) {
-            result.add(i * 2)
-        }
-        return result
-    }
+    fun solution(numbers: IntArray) = numbers.map { it * 2 }
 }
