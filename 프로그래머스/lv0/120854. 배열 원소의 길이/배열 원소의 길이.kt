a2d0d97790c1134a324d0ed9f405736b172e0e76
Danbi <76150928+itssweetrain@users.noreply.github.com>
2023-05-12 23:59:35 +0900

    [level 0] Title: 배열 원소의 길이, Time: 5.57 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.kt
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.kt
@@ -1,11 +1,3 @@
 class Solution {
-    fun solution(strlist: Array<String>): ArrayList<Int> {
-        var result = ArrayList<Int>()
-        var len = 0
-        
-        for(i in strlist) {
-            result.add(i.length)   
-        }
-        return result
-    }
+    fun solution(strlist: Array<String>): IntArray = strlist.map { it.length }.toIntArray()
 }
