35995f6f6e18b5f5e5e690e0a2e5411917d461dc
Danbi <76150928+itssweetrain@users.noreply.github.com>
2023-07-28 23:39:08 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.kt
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(money: Int): ArrayList<Int> {
-       val count = money / 5500
-       val changes = money % 5500
-       var result = ArrayList<Int>()
-       result.add(count)
-       result.add(changes)
-       return result
-    }
+    fun solution(money: Int) = intArrayOf(money / 5500, money % 5500)
 }
