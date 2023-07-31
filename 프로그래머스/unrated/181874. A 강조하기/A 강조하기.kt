b820baaf40338f174a5e6253e4d9fb5e47d3524c
Danbi <76150928+itssweetrain@users.noreply.github.com>
2023-07-31 23:41:05 +0900

    [unrated] Title: A 강조하기, Time: 6.80 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.kt
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.kt
@@ -1,10 +1,5 @@
 class Solution {
     fun solution(myString: String): String {
-        var answer: String = ""
-        for(i in myString) {
-            val str = i.toString()
-            if(str == "a") answer += "A" else if(str != "A") answer += str.toLowerCase() else answer += str
-        }
-        return answer
+        return myString.lowercase().replace("a", "A")
     }
 }
